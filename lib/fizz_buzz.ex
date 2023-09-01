4ad77fabca292afebf5df423635d206461fdacdc
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-01 15:22:49 -0300

    adicionado guards para resolução problema

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,10 +6,23 @@ defmodule FizzBuzz do
     |> handle_verify_file()
   end
 
-  def handle_verify_file({:ok, result}) do
-    result
+  defp handle_verify_file({:ok, result}) do
+    result = result
     |> String.split(",")
-    |> Enum.map(fn items -> String.to_integer(items) end)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
+
+    {:ok, result}
   end
-  def handle_verify_file({:error, reason}), do: "Error read file: #{reason}"
+  defp handle_verify_file({:error, reason}), do: {:error, "Error read file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzBuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :Buzz
+  defp evaluate_numbers(number), do: number
 end
